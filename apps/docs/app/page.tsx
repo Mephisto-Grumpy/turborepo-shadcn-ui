19e67cb148bff5c3967392d3c24c065a35846025
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-09-23 10:52:53 +0000

    add card and test it

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,26 @@
 import { Button } from "@repo/ui/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@repo/ui/components/ui/card'
 
 export default function Page() {
   return (
     <main>
       <Button>Click me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Test Card</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription>
+            It's work or not!
+          </CardDescription>
+        </CardContent>
+      </Card>
     </main>
   );
 }
